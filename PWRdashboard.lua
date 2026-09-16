940c31775c8f8c79297a7bb86091b523a27b428d
Rory on Mac <rorydeller@gmail.com>
2026-09-16 02:45:12 +0100

    Drafting function behaviour for fetching decay

--- PWRdashboard.lua
+++ PWRdashboard.lua
@@ -1,10 +1,43 @@
 local component = require("component")
 local event = require("event")
 
-
 local gpu = component.gpu
 local Pgauge = component.ntm_power_gauge
 local Sgauge = component.ntm_fluid_gauge
 local PWR = component.ntm_pwr_control
 
-local total_Flux = 
+local totalFlux = PWR.getFlux() 
+local TU = totalFlux * 7.5 
+
+function getFuelData() -- 3 indexes 1: Fuel count, 2: Fuel depletion, 3: Max Depletion
+    local table = {}
+    for item in PWR.getFuelInfo() do
+        table.insert(fuelTable, item)
+    end
+    return table
+end 
+
+function fluxDecay(fuelData)
+    local count = fuelData[1]
+    local depletion = fuelData[2]
+    local maxDepletion = fuelData[3]
+    local decay =  1 - (depletion / maxDepletion)
+    if decay == 0 then
+        return nil
+    else 
+        local instance = totalFlux / count
+        local stat = instance * decay
+        return stat
+    end
+end
+
+
+function getReactionInput(Y, D) --Placeholder for parabola involved in collisions, seperate page graph entirely
+    local x = (Y/(25*D))^2
+    return x
+end
+
+while true
+    snapshot = fluxDecay(getFuelData())
+    totalFlux = totalFlux - snapshot
+end

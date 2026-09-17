18707ae0070a848c2816fc8f73e6f3bacc393fa3
Rory Deller <rorydeller@gmail.com>
2026-09-18 00:29:45 +0100

    Finalised Measurements before Plotting

--- PWRdashboard.lua
+++ PWRdashboard.lua
@@ -5,41 +5,38 @@ local gpu = component.gpu
 local Pgauge = component.ntm_power_gauge
 local Sgauge = component.ntm_fluid_gauge
 local PWR = component.ntm_pwr_control 
+local flux
 
 function getFuelData() -- 3 indexes 1: Fuel count, 2: Fuel depletion, 3: Max Depletion
-    local table = {}
-    for item in PWR.getFuelInfo() do
-        table.insert(fuelTable, item)
-    end
-    return table
+    local fuelTable = {PWR.getFuelInfo()}
+    return fuelTable
 end 
 
 function fluxDecay(fuelData)
-    local count = fuelData[1]
+    local rods = fuelData[1]
     local depletion = fuelData[2]
     local maxDepletion = fuelData[3]
-    local decay =  1 - (depletion / maxDepletion)
-    if decay == 0 then
-        return nil
-    else 
-        local instance = totalFlux / count
-        local stat = instance * decay
-        return stat
-    end
+    local percent = (depletion / maxDepletion) * 100  --Percentage of current fuel rod depleted of flux
+    local instance = flux / rods                
+    local stat = instance * (percent/100)
+    return stat, percent
 end
 
-function getReactionInput(Y, D) --Placeholder for parabola involved in collisions, seperate page graph entirely
-    local x = (Y/(25*D))^2
+function getReactionInput(Y) --Parabola input in collisions, seperate page graph entirely
+    local x = (1/625)*Y^2
     return x
 end
 
-while true
-    local Flux = PWR.getFlux() 
-    local TU = totalFlux * 7.5
+while true do
+    flux = PWR.getFlux() 
+    local TU = flux * 7.5
     local fuelInfo = getFuelData()
-    local snapshot = fluxDecay(getFuelData())
-    local newFlux = totalFlux - snapshot
-
-    print("Dial flux:" .. Flux .. "Actual flux:" .. newFlux)
-    print("Rods remaining:" .. fuelInfo[1] .. "Current rod depletion:" .. snapshot)
+    local snapshot, depletion = fluxDecay(getFuelData())
+    local totalFuel = fuelInfo[1] - (depletion / 100)
+    local Y = flux - snapshot
+    local X = getReactionInput(Y) 
+    print("Current flux redacted " .. math.floor(snapshot) .. "     At " .. math.floor(depletion) .. "%" .. "     Totalling fuel at " .. totalFuel)
+    print(" Rods Remaining: " .. fuelInfo[1] .. "     Dial flux: " .. math.floor(flux) .. "     Actual flux: " .. math.floor(Y))
+    print(" Input flux during reactivity " .. X)
+    os.sleep(15)
 end

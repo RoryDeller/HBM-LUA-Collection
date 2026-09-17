bdbed0e1c6717b0015520d22aecd22cbc1cfb37b
Rory on Mac <rorydeller@gmail.com>
2026-09-17 19:25:32 +0100

    testing branch

--- PWRdashboard.lua
+++ PWRdashboard.lua
@@ -4,10 +4,7 @@ local event = require("event")
 local gpu = component.gpu
 local Pgauge = component.ntm_power_gauge
 local Sgauge = component.ntm_fluid_gauge
-local PWR = component.ntm_pwr_control
-
-local totalFlux = PWR.getFlux() 
-local TU = totalFlux * 7.5 
+local PWR = component.ntm_pwr_control 
 
 function getFuelData() -- 3 indexes 1: Fuel count, 2: Fuel depletion, 3: Max Depletion
     local table = {}
@@ -31,13 +28,18 @@ function fluxDecay(fuelData)
     end
 end
 
-
 function getReactionInput(Y, D) --Placeholder for parabola involved in collisions, seperate page graph entirely
     local x = (Y/(25*D))^2
     return x
 end
 
 while true
+    local Flux = PWR.getFlux() 
+    local TU = totalFlux * 7.5
+    local fuelInfo = getFuelData()
     local snapshot = fluxDecay(getFuelData())
     local newFlux = totalFlux - snapshot
+
+    print("Dial flux:" .. Flux .. "Actual flux:" .. newFlux)
+    print("Rods remaining:" .. fuelInfo[1] .. "Current rod depletion:" .. snapshot)
 end
